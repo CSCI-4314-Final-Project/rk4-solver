cb7f4af3a6cb49ca2e2ef164ed88ba33cf1bc12d
Sidhant Puntambekar <sidhantnp@yahoo.com>
2023-03-14 17:33:47 -0600

    Added docstrings for cell population ODE functions

--- rk4_solver.py
+++ rk4_solver.py
@@ -3,7 +3,58 @@ import pandas as pd
 import matplotlib as plt
 import scipy.special as sp
 
+def tumor_cell_population_function():
+    """
+    Creates a differential equation to model a tumor cell population over time
+    
+    Parameters: TODO
+    ----------
+    num1 : int
+        First number to add.
+    num2 : int
+        Second number to add.
+
+    Returns: TODO
+    -------
+    int
+        The sum of ``num1`` and ``num2``.
+    """
+    return 0
+
+def effector_cell_population_function():
+    """
+    Creates a differential equation to model an effector cell population over time
+    
+    Parameters
+    ----------
+    num1 : int
+        First number to add.
+    num2 : int
+        Second number to add.
+
+    Returns
+    -------
+    int
+        The sum of ``num1`` and ``num2``.
+    """
+    return 0
+
 def rk4(x_0, y_0, x_n, num_steps):
+    """
+    Solves a (set of) differential equations through the Runge-Kutta (RK4) method
+    
+    Parameters: TODO
+    ----------
+    num1 : int
+        First number to add.
+    num2 : int
+        Second number to add.
+
+    Returns: TODO
+    -------
+    int
+        The sum of ``num1`` and ``num2``.
+    """
     return 0
 
 if __name__ == "__main__":
