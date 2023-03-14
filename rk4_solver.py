29a95dbd6523dc40dddf157992cc577b24dad074
Sidhant Puntambekar <sidhantnp@yahoo.com>
2023-03-13 21:47:51 -0600

    Made initial rk4 solver python file

---
+++ rk4_solver.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import scipy.special as sp
+
+def rk4(x_0, y_0, x_n, num_steps):
+    return 0
+
+if __name__ == "__main__":
+    print("hello")
