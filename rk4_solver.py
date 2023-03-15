cea37311fe5811a29478c62c6a7eca3572c7ff40
Sidhant Puntambekar <sidhantnp@yahoo.com>
2023-03-14 21:03:09 -0600

    Added rk4 solver code

--- rk4_solver.py
+++ rk4_solver.py
@@ -39,23 +39,41 @@ def effector_cell_population_function():
     """
     return 0
 
-def rk4(x_0, y_0, x_n, num_steps):
+def rk4(x_start, x_finish, init_condition, num_steps, function):
     """
     Solves a (set of) differential equations through the Runge-Kutta (RK4) method
     
-    Parameters: TODO
+    Parameters:
     ----------
-    num1 : int
-        First number to add.
-    num2 : int
-        Second number to add.
-
-    Returns: TODO
+    start : float
+        Starting point of the numerical sim
+    finish : float
+        End point of the numerical sim
+    num_steps : int
+        Number of steps to take
+    init_condition : float
+        Initial condition for the IVP of the ODE
+    
+    Returns:
     -------
     int
-        The sum of ``num1`` and ``num2``.
+        Approximation w to function (y) at the (num_steps + 1) value of x.
     """
-    return 0
+    step_size = abs(x_finish - x_start) / num_steps # Defines the step size to take when solving rk4 numerically
+    x = [x_start] # Store x values as we compute them, with x_start being first
+    y = [init_condition] # Store y values as we compute them, with init_condition being first
+
+    for i in range(num_steps): # Loop through the number of steps to approximate solution
+        # https://mathworld.wolfram.com/Runge-KuttaMethod.html
+        k1 = step_size * function(x[i], y[i])
+        k2 = step_size * function(x[i] + (step_size / 2), y[i] + (k1 / 2))
+        k3 = step_size * function(x[i] + (step_size / 2), y[i] + (k2 / 2))
+        k4 = step_size * function(x[i], y[i] + k3)
+
+        x.append(x_start + (i + 1) * step_size) # Store new computed x
+        y.append(y[i] + (k1 + (2 * k2) + (2 * k3) + k4) / 6) # Store new computed y
+    
+    return (x, y) # Return final (x, y) lists
 
 if __name__ == "__main__":
     print("hello")
