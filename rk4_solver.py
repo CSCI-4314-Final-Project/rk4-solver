a595f6e27b230ffabc2da9a67d0ea83c5d60f726
Stefan Wasshuber <stefan.wasshuber@gmail.com>
2023-03-16 18:25:12 -0600

    created diff eqs

--- rk4_solver.py
+++ rk4_solver.py
@@ -3,41 +3,100 @@ import pandas as pd
 import matplotlib as plt
 import scipy.special as sp
 
-def tumor_cell_population_function():
+def tumor_cell_population_function(T,E,M):
     """
     Creates a differential equation to model a tumor cell population over time
     
     Parameters: TODO
     ----------
-    num1 : int
-        First number to add.
-    num2 : int
-        Second number to add.
-
+    r : float
+        rate of tumor growth
+    T : float
+        number of tumor cells for time t
+    b : float
+        capacity of the tumor cell
+    a : float
+        paramter of cancer cleanup
+    E : float
+        number of effector cells at time t
+    g : float
+        half-saturation for cancer cleanup
+    K_t : float
+        rate of tumor cell death by chemotherapy drug
+    M : float
+        concentration of chemotherapy drug at time t
     Returns: TODO
     -------
-    int
-        The sum of ``num1`` and ``num2``.
+    float
+        change in tumor cells after one timestep
     """
-    return 0
+    r = 4.31 * 10**(-3)
+    b = 10**(-9)
+    a = 3.41 * 10**(-10)
+    g = 10**5
+    K_t = 1 # not sure what this is yet
+    dT = r*T*(1-(b*T))-a*((E*T)/(T+g))-(K_t*M*T)
+    return dT
 
-def effector_cell_population_function():
+def effector_cell_population_function(T,E,M):
     """
     Creates a differential equation to model an effector cell population over time
     
     Parameters: TODO
     ----------
-    num1 : int
-        First number to add.
-    num2 : int
-        Second number to add.
-
+    s : float
+        Growth rate of normal / effector cells
+    mu : float
+        Rate of natural demise of effector cells
+    E : int
+        Number of effector cells at time t
+    p : float
+        degree of recruitment of maximum immune-effector cells
+    h : int
+        steepness coefficient of the recruitment curve of effectorimmune cells 
+    T : int
+        Number of tumor cells at time t
+    m : float
+        degree of inactivation of effector cells by tumor cells
+    M : int
+        concentration fo chemotherapy drug
+    K_e : float
+        rate at which chemotherapy drugs kill effector cells
     Returns: TODO
     -------
-    int
-        The sum of ``num1`` and ``num2``.
+    float
+        The change in efffector cells after one timestep
     """
-    return 0
+    s = 1.2 * 10**4
+    mu = 4.12 * 10**(-2)
+    p = 0.015
+    h = 20.2
+    m = 2 * 10**(-11)
+    K_e = 1 # not sure what this is yet
+    dE = s-(mu*E) + p*((E*T)/(h+T)) - (m*E*T) - (K_e*M*E)
+    return dE
+
+def chemotherapy_drug_concentration_function(M):
+    """
+    Creates a differential equation to model the concentration of chemotherapy drug over time
+    
+    Parameters: TODO
+    ----------
+    gamma : float
+        rate of decrease in concentration of chemotherapy drug
+    M : int
+        concentration fo chemotherapy drug
+    V_m : float
+        outside addition of drug, in the litterature, this is a function, not sure how it is a function of time. 
+    Returns: TODO
+    -------
+    float
+        The change in chemotherapy drug concentration
+    """
+    gamma = 0.9
+    V_m = 1 # not sure what this is yet
+    dM = -gamma*M+V_m
+    return dM
 
 def rk4(x_start, x_finish, init_condition, num_steps, function):
     """
